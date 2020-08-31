93ffd3ed85da5eac78c962aead32899f1674ed90
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-31 00:45:44 -0400

    Consistency sellSnack -> sellSnacks, buySnack ditto

--- snackbar/Snack.java
+++ snackbar/Snack.java
@@ -53,11 +53,11 @@ public class Snack {
 		this.vendingMachineId = vendingMachineId;
 	}
 
-	public void sellSnack(int num) {
+	public void sellSnacks(int num) {
 		this.quantity = quantity - num;
 	}
 
-  public void stockSnack(int num) {
+  public void stockSnacks(int num) {
 		this.quantity = quantity + num;
 	}
 }
