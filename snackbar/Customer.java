b649f278dd582f2a843a53f057c1d2caf652b9c4
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-30 23:56:05 -0400

    Create Customer class

---
+++ snackbar/Customer.java
@@ -0,0 +1,17 @@
+package snackbar;
+
+public class Customer {
+	// fields
+	private static int maxId = 0;
+	private int id;
+	private String name;
+	private double cash;
+
+	// constructor
+	public Customer(String name, double cash) {
+		maxId++;
+		id = maxId;
+		this.name = name;
+		this.cash = cash;
+	}
+}
